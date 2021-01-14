428d99e4a796fdb8c2958b61f8dc3cf42f767547
BaslaHassine <turkibaslahassine@gmail.com>
2021-01-14 16:37:41 +0100

    still

--- shop.js
+++ shop.js
@@ -5,7 +5,7 @@ var createAccount = function(){
     if(document.getElementById("firstName").value && document.getElementById("lastName").value && document.getElementById("email").value && document.getElementById("pass").value) {
         if (document.getElementById("pass").value !== document.getElementById("confirmPass").value) {
             document.getElementById("message").style.backgroundColor = "orange"
-            document.getElementById("message").style.color = "red"
+            document.getElementById("message").style.color = "black"
             document.getElementById("message").innerText="Check your password...!";
         } 
         else {
@@ -14,13 +14,16 @@ var createAccount = function(){
             user.email = document.getElementById("email").value;
             user.pass = document.getElementById("pass").value;
             document.getElementById("message").style.backgroundColor = "lightgreen"
-            document.getElementById("message").style.color = "green"
+            document.getElementById("message").style.color = "black"
             document.getElementById("message").innerText = "Thank you " + user.firstName + " " + user.lastName + ". Your account successfully created.";
+            window.localStorage.setItem('E-mail', user.email);
+            window.localStorage.setItem("Password", user.pass);
         }
     }
     else {
-        document.getElementById("message").style.backgroundColor = "orange"
-        document.getElementById("message").style.color = "red"
+        document.getElementById("message").style.backgroundColor = "red"
+        document.getElementById("message").style.color = "black"
         document.getElementById("message").innerText="Please fill up the form to proceed...!";
     }
 }
+
