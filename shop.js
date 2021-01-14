1818a1fac8d73fe9dc321116feb816129bcef85e
noomenkrichen <nkrichen@outlook.com>
2021-01-14 14:36:48 +0100

    fixed the create account function in .js and .html files

--- shop.js
+++ shop.js
@@ -1,21 +1,26 @@
-window.onload= function(){
 
-}
-
-
-var checkPass = function(){
-    
-}
 
 var createAccount = function(){
     var user = {};
-    if (document.getElementById("pass").value !== document.getElementById("confirmPass").value) {
-        document.getElementById("message").innerText="Check your password";
-    } 
+    if(document.getElementById("firstName").value && document.getElementById("lastName").value && document.getElementById("email").value && document.getElementById("pass").value) {
+        if (document.getElementById("pass").value !== document.getElementById("confirmPass").value) {
+            document.getElementById("message").style.backgroundColor = "orange"
+            document.getElementById("message").style.color = "red"
+            document.getElementById("message").innerText="Check your password...!";
+        } 
+        else {
+            user.firstName = document.getElementById("firstName").value;
+            user.lastName = document.getElementById("lastName").value;
+            user.email = document.getElementById("email").value;
+            user.pass = document.getElementById("pass").value;
+            document.getElementById("message").style.backgroundColor = "lightgreen"
+            document.getElementById("message").style.color = "green"
+            document.getElementById("message").innerText = "Thank you " + user.firstName + " " + user.lastName + ". Your account successfully created.";
+        }
+    }
     else {
-        user.login = document.getElementById("login").value;
-        user.email = document.getElementById("email").value;
-        user.pass = document.getElementById("pass").value;
-        document.getElementById("message").innerText="Thank you "+user.login+"Your account successfully created";
+        document.getElementById("message").style.backgroundColor = "orange"
+        document.getElementById("message").style.color = "red"
+        document.getElementById("message").innerText="Please fill up the form to proceed...!";
     }
 }
