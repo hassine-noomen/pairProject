136cc961516b5acb2eebcc59988a55d7c0518b4a
noomenkrichen <nkrichen@outlook.com>
2021-01-14 12:59:20 +0100

    fixed index.html account creation function

--- shop.js
+++ shop.js
@@ -1,13 +1,21 @@
 window.onload= function(){
 
+}
 
-    var checkPass = function(){
-        if (document.getElementById("pass").value !== document.getElementById("confirmPass").value) {
-        console.log(document.getElementById("pass").value);
-        console.log(document.getElementById("confirmPass").value);
-        document.getElementById("message").value = "Check your password";
-        }
+
+var checkPass = function(){
+    
+}
+
+var createAccount = function(){
+    var user = {};
+    if (document.getElementById("pass").value !== document.getElementById("confirmPass").value) {
+        document.getElementById("message").innerText="Check your password";
+    } 
+    else {
+        user.login = document.getElementById("login").value;
+        user.email = document.getElementById("email").value;
+        user.pass = document.getElementById("pass").value;
+        document.getElementById("message").innerText="Thank you "+user.login+"Your account successfully created";
     }
-
-
 }
